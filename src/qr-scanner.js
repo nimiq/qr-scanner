0cc9ee284123eefd526dc4d2a0dada45289fd82e
Daniel <github.danimoh@neverbox.com>
2019-02-05 09:42:17 -0600

    change default QrScanner.WORKER_PATH; fix WORKER_PATH in demo

--- src/qr-scanner.js
+++ src/qr-scanner.js
@@ -312,5 +312,4 @@ export default class QrScanner {
     }
 }
 QrScanner.DEFAULT_CANVAS_SIZE = 400;
-QrScanner.WORKER_PATH = '/libraries/qr-scanner/qr-scanner-worker.min.js';
-/** @preserve @asset(/libraries/qr-scanner/qr-scanner-worker.min.js) */
+QrScanner.WORKER_PATH = 'qr-scanner-worker.min.js';
